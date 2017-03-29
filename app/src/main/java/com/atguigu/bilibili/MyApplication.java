3f998e4b4aa730581311eda6050e8081d6ab82b8
ITLC <1740104885@qq.com>
2017-03-29 18:15:39 +0800

    极光推送

--- app/src/main/java/com/atguigu/bilibili/MyApplication.java
+++ app/src/main/java/com/atguigu/bilibili/MyApplication.java
@@ -3,18 +3,24 @@ package com.atguigu.bilibili;
 import android.app.Application;
 import android.content.Context;
 
+import cn.jpush.android.api.JPushInterface;
+
 
 public class MyApplication extends Application {
 
-    private static  Context mContext;
+    private static Context mContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
         mContext = this;
+
+        JPushInterface.setDebugMode(true);    // 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);            // 初始化 JPush
     }
 
-    public static  Context getInstance(){
+    // 初始化 JPush
+    public static Context getInstance() {
         return mContext;
     }
 }
